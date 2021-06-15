239592841315bf82fe28e904eb8b6e6d9c926bc6
Ryan <ryanjakegarcia@gmail.com>
2021-06-15 15:08:30 -0700

    Testing Class for Tyler

---
+++ RollerTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using Character_Generator;
+
+namespace RollerTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            AutoRoller roller = new AutoRoller("4d6d1");
+            roller.Roll();
+        }
+    }
+}
